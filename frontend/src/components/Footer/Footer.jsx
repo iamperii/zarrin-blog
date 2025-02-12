6a6851faead5969e93bfe7cdfdb5a267a65d9fa1
Memmedova Peri <perimemmedova20052022@gmail>
2025-02-12 17:46:42 +0400

    Added header and footer for desktop version

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import style from './footer.module.scss';
+import ZarrinLogo from '../../assets/svg/zarrin-logo.svg';
+import { Link } from 'react-router';
+import { CiFacebook } from 'react-icons/ci';
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
-		<div>Footer</div>
-	)
-}
+		<>
+			<div className={style.footer}>
+				<div className={style['first-section']}>
+					<div className={style.logo}>
+						<img src={ZarrinLogo} alt="zarrin-logo" />
+						<h1 className={style.title}>Zarrin</h1>
+					</div>
+					<div className={style['nav-items']}>
+						<Link to="/" className={style['nav-item']}>
+							Home
+						</Link>
+						<Link to="/blogs" className={style['nav-item']}>
+							Blog
+						</Link>
+						<Link to="/about" className={style['nav-item']}>
+							About
+						</Link>
+						<Link to="/contact" className={style['nav-item']}>
+							Contact Us
+						</Link>
+					</div>
+					<div className={style['social-icons']}>
+						<FaFacebook className={style['social-icon']} />
+						<FaLinkedin className={style['social-icon']} />
+						<FaYoutube className={style['social-icon']} />
+						<FaInstagram className={style['social-icon']} />
+					</div>
+				</div>
 
-export default Footer
+				<div className={style.copyright}>
+					<p className={style['copyright-info']}>
+						Copyright Ideapeel Inc © 2023. All Right Reserved
+					</p>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Footer;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import style from './header.module.scss';
+import ZarrinLogo from '../../assets/svg/zarrin-logo.svg';
+import { CiSearch } from 'react-icons/ci';
+import { Link } from 'react-router';
 
 const Header = () => {
 	return (
-		<div>Header</div>
-	)
-}
+		<>
+			<div className={style.header}>
+				<div className={style.logo}>
+					<img src={ZarrinLogo} alt="zarrin-logo" />
+					<h1 className={style.title}>Zarrin</h1>
+				</div>
+				<div className={style['nav-items']}>
+					<Link to="/blogs" className={style['nav-item']}>
+						Blog
+					</Link>
+					<Link to="/about" className={style['nav-item']}>
+						About
+					</Link>
+					<div className={style['nav-item']}>
+						<CiSearch size={30} />
+					</div>
+					<Link to="/contact" className={style['nav-item']}>
+						Contact Us
+					</Link>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default Header
+export default Header;
