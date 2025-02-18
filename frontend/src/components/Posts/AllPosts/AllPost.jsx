173291c6de1d2d9fb444071666f52d6e0027a443
Memmedova Peri <perimemmedova20052022@gmail>
2025-02-18 15:26:18 +0400

    All Posts component done.

--- frontend/src/components/Posts/AllPosts/AllPost.jsx
+++ frontend/src/components/Posts/AllPosts/AllPost.jsx
@@ -1,199 +1,44 @@
 import style from './allPosts.module.scss';
-import Container from '../../../assets/images/Container.png';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAllPosts } from '../../../store/actions/postAction';
+import { formatDate } from '../../../utils/dateFormatter';
+
 const AllPost = () => {
+	const { posts, loading, error } = useSelector((state) => state.posts);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchAllPosts());
+	}, [dispatch]);
+
 	return (
 		<>
 			<div className={style.allPost}>
+				{loading && <p className={style.loading}>Loading...</p>}
 				<div className={style.cards}>
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-					<div className={style.card}>
-						<div className={style['card-img']}>
-							<img src={Container} alt="" />
-						</div>
-						<div className={style['card-section-first']}>
-							<div className={style['card-content']}>Travel</div>
-							<div className={style['card-date']}> 13 March 2023</div>
-						</div>
-						<div className={style['card-title']}>
-							<h1>Train Or Bus Journey?Which one suits?</h1>
-						</div>
-						<div className={style['card-desc']}>
-							<p>
-								The choice between a train or bus journey depends on various
-								factors such as the distance of the journey, the time available,
-								the cost, and person
-							</p>
-						</div>
-						<Link to="/blog/1">Read more...</Link>
-					</div>
-
-
-					
+					{posts?.map((post) => {
+						return (
+							<div className={style.card} key={post.id}>
+								<div className={style['card-img']}>
+									<img src={post.img_1} alt="" />
+								</div>
+								<div className={style['card-section-first']}>
+									<div className={style['card-content']}>{post.content}</div>
+									<div className={style['card-date']}>
+										{formatDate(post.date)}
+									</div>
+								</div>
+								<div className={style['card-title']}>
+									<h1>{post.title}</h1>
+								</div>
+								<div className={style['card-desc']}>
+									<p>{post.short_desc}</p>
+								</div>
+								<Link to={`/blog/${post.id}`}>Read more...</Link>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</>
